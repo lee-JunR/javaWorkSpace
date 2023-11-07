e466654fd7b65a9ebe5f49578b9a26de573aebda
lee-JunR <wns2610@icloud.com>
2023-11-07 12:15:13 +0900

    feat : "Iterator 연습 자바 클래스 추가"

--- src/ch_09/_08_Iterator.java
+++ src/ch_09/_08_Iterator.java
@@ -1,8 +1,11 @@
 package ch_09;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
 
 public class _08_Iterator {
 
@@ -42,9 +45,9 @@ public class _08_Iterator {
     System.out.println("--------------------------------");
 
     it = list.iterator();
-    while(it.hasNext()) {
+    while (it.hasNext()) {
       String s = it.next();
-      if (s.contains("알 수 없음")){
+      if (s.contains("알 수 없음")) {
         it.remove(); // 삭제
       }
     }
@@ -53,6 +56,41 @@ public class _08_Iterator {
       System.out.println(it.next());
     }
     System.out.println("--------------------------------");
+
+    // Set 이나 Map 에서도 Iterator 사용 가능
+    System.out.println("HashSet Iterator 순회");
+    HashSet<String> set = new HashSet<>();
+    Iterator<String> itSet = set.iterator();
+    while (itSet.hasNext()) {
+      System.out.println(itSet.next());
+    }
+    System.out.println("--------------------------------");
+
+    HashMap<String, Integer> map = new HashMap<>();
+    map.put("유재석", 10);
+    map.put("박명수", 3);
+    map.put("노홍철", 5);
+
+    // Iterator<String> itMapKey = map.iterator(); // 제공 X
+    System.out.println("HashMap Iterator 순회");
+
+    System.out.println("---key 순회 : map.keySet().iterator()---");
+    Iterator<String> itMapKey = map.keySet().iterator();
+    while (itMapKey.hasNext()) {
+      System.out.println(itMapKey.next());
+    }
+    System.out.println("---Value 순회 : map.keySet().iterator()---");
+
+    Iterator<Integer> itMapValue = map.values().iterator();
+    while (itMapValue.hasNext()) {
+      System.out.println(itMapValue.next());
+    }
+    System.out.println("---Key, Value 같이 순회---");
+    Iterator<Entry<String, Integer>> itMap = map.entrySet().iterator();
+    while (itMap.hasNext()) {
+      System.out.println(itMap.next());
+    }
+
   }
 
 }
